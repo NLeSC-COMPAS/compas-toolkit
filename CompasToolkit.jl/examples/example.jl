49052d2918a440d7d34506370c7317ee0c01de03
stijn <s.heldens@esciencecenter.nl>
2023-07-14 12:46:49 +0200

    Update bindings and example with new toolkit functions

--- CompasToolkit.jl/examples/example.jl
+++ CompasToolkit.jl/examples/example.jl
@@ -1,4 +1,52 @@
-import CompasToolkit
+using CompasToolkit
 
-print(names(CompasToolkit))
-CompasToolkit.make_context(1)
+context = CompasToolkit.make_context(0)
+
+# First we assemble a Shepp Logan phantom with homogeneous T₁ and T₂
+# but non-constant proton density and B₀
+N = 256
+ρ = complex.(ImagePhantoms.shepp_logan(N, ImagePhantoms.SheppLoganEmis())');
+T₁ = fill(0.85, N, N);
+T₂ = fill(0.05, N, N);
+B₀ = repeat(1:N,1,N);
+
+# We also set the spatial coordinates for the phantom
+FOVˣ, FOVʸ = 25.6, 25.6;
+X = [x for x ∈ LinRange(-FOVˣ/2, FOVˣ/2, N), y ∈ 1:N];
+Y = [y for x ∈ 1:N, y ∈ LinRange(-FOVʸ/2, FOVʸ/2, N)];
+
+# Finally we assemble the phantom as an array of `T₁T₂B₀ρˣρʸxy` values
+parameters = CompasToolkit.make_tissue_parameters(context, T₁, T₂, B₀, real.(ρ), imag.(ρ), X, Y)
+
+
+# Next, we assemble a Cartesian trajectory with linear phase encoding
+nr = N # nr of readouts
+ns = N # nr of samples per readout
+Δt_adc = 10^-5 # time between sample points
+py = -(N÷2):1:(N÷2)-1 # phase encoding indices
+Δkˣ = 2π / FOVˣ; # k-space step in x direction for Nyquist sampling
+Δkʸ = 2π / FOVʸ; # k-space step in y direction for Nyquist sampling
+k0 = [(-ns/2 * Δkˣ) + im * (py[mod1(r,N)] * Δkʸ) for r in 1:nr]; # starting points in k-space per readout
+Δk = [Δkˣ + 0.0im for r in 1:nr]; # k-space steps per sample point for each readout
+
+trajectory = CompasToolkit.make_cartesian_trajectory(context, nr, ns, Δt_adc, k0, Δk);
+
+
+# We use two different receive coils
+coil₁ = complex.(repeat(LinRange(0.5,1.0,N),1,N));
+coil₂ = coil₁';
+coil₃ = coil₂;
+coil₄ = coil₂;
+
+coil_sensitivities = hcat(coil₁, coil₂, coil₃, coil₄)
+
+signal = zeros(ComplexF32, N*N, 4)
+echos = zeros(ComplexF32, N*N, nr)
+
+CompasToolkit.simulate_signal(
+    context,
+    signal,
+    echos,
+    parameters,
+    trajectory,
+    coil_sensitivities)
--- julia-bindings/src/bindings.cpp
+++ julia-bindings/src/bindings.cpp
@@ -1,7 +1,135 @@
 #include "context.h"
+#include "trajectory.h"
 #include "jlcxx/jlcxx.hpp"
+#include "tissueparameters.h"
+#include "simulate_signal.h"
+
+template <typename T, int N>
+compas::host_view<T, N> into_view(const jlcxx::ArrayRef<T, N>& array) {
+    compas::fixed_array<int, N> shape;
+    for (int i = 0; i < N; i++) {
+        shape[i] = jl_array_dim(array.wrapped(), i);
+    }
+
+    return {array.data(), shape};
+}
+
+template <typename T, int N>
+compas::host_view<compas::complex_type<T>, N> into_view(const jlcxx::ArrayRef<std::complex<T>, N>& array) {
+    compas::fixed_array<int, N> shape;
+
+    for (int i = 0; i < N; i++) {
+        shape[i] = jl_array_dim(array.wrapped(), N - i - 1);
+    }
+
+    auto ptr = static_cast<const compas::complex_type<T>*>(static_cast<const void*>(array.data()));
+    return {ptr, shape};
+}
+
 
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod) {
     mod.add_type<compas::CudaContext>("CudaContext");
     mod.method("make_context", compas::make_context);
+
+
+    mod.add_type<compas::CartesianTrajectory>("CartesianTrajectory");
+    mod.method("make_cartesian_trajectory", [](
+        const compas::CudaContext& context,
+        int nreadouts,
+        int samples_per_readout,
+        float delta_t,
+        jlcxx::ArrayRef<std::complex<float>> k_start,
+        jlcxx::ArrayRef<std::complex<float>> delta_k
+    ) {
+        return make_cartesian_trajectory(
+                context, nreadouts, samples_per_readout,
+                delta_t,
+                into_view(k_start),
+                into_view(delta_k)
+        );
+    });
+
+    mod.add_type<compas::TissueParameters>("TissueParameters");
+    mod.method("make_tissue_parameters", [](
+            const compas::CudaContext& context,
+            int nvoxels,
+            jlcxx::ArrayRef<float> T1,
+            jlcxx::ArrayRef<float> T2,
+            jlcxx::ArrayRef<float> B1,
+            jlcxx::ArrayRef<float> B0,
+            jlcxx::ArrayRef<float> rho_x,
+            jlcxx::ArrayRef<float> rho_y,
+            jlcxx::ArrayRef<float> x,
+            jlcxx::ArrayRef<float> y,
+            jlcxx::ArrayRef<float> z
+    ) {
+        return make_tissue_parameters(
+                context,
+                nvoxels,
+                into_view(T1),
+                into_view(T2),
+                into_view(B1),
+                into_view(B0),
+                into_view(rho_x),
+                into_view(rho_y),
+                into_view(x),
+                into_view(y),
+                into_view(z)
+            );
+    });
+
+    mod.method("make_tissue_parameters", [](
+            const compas::CudaContext& context,
+            int nvoxels,
+            jlcxx::ArrayRef<float> T1,
+            jlcxx::ArrayRef<float> T2,
+            jlcxx::ArrayRef<float> B1,
+            jlcxx::ArrayRef<float> B0,
+            jlcxx::ArrayRef<float> rho_x,
+            jlcxx::ArrayRef<float> rho_y,
+            jlcxx::ArrayRef<float> x,
+            jlcxx::ArrayRef<float> y
+    ) {
+        return make_tissue_parameters(
+                context,
+                nvoxels,
+                into_view(T1),
+                into_view(T2),
+                into_view(B1),
+                into_view(B0),
+                into_view(rho_x),
+                into_view(rho_y),
+                into_view(x),
+                into_view(y),
+                {}
+        );
+    });
+
+
+    mod.method("simulate_signal", [](
+            const compas::CudaContext& context,
+            jlcxx::ArrayRef<std::complex<float>, 3> julia_signal,
+            jlcxx::ArrayRef<std::complex<float>, 2> julia_echos,
+            compas::TissueParameters parameters,
+            compas::CartesianTrajectory trajectory,
+            jlcxx::ArrayRef<std::complex<float>, 2> julia_coil_sensitivities
+    ) {
+        auto signal = into_view(julia_signal);
+        auto echos = into_view(julia_echos);
+        auto coil_sensitivities = into_view(julia_coil_sensitivities);
+
+        auto d_signal = context.allocate(signal);
+        auto d_echos = context.allocate(echos);
+        auto d_coil_sensitivities = context.allocate(coil_sensitivities);
+
+        compas::simulate_signal(
+                context,
+                d_signal,
+                d_echos,
+                parameters,
+                trajectory,
+                d_coil_sensitivities);
+
+//        d_signal.copy_to(signal);
+    });
 }
