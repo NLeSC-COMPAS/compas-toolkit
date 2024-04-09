0058a42a0c05deff6f81bf191bf9834050bce040
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2024-04-09 13:42:25 +0200

    Fix a bug in the kernel.

--- src/simulate/phase_encoding_kernels.cuh
+++ src/simulate/phase_encoding_kernels.cuh
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "core/view.h"
+#include "parameters/tissue_view.cuh"
 
 namespace compas {
 namespace kernels {
 
 template<typename TrajectoryView>
 __global__ void phase_encoding(
-    cuda_view<cfloat, 2> echos,
+    cuda_view_mut<cfloat, 2> echos,
     TissueParametersView parameters,
     TrajectoryView trajectory) {
     auto readout = index_t(blockIdx.y * blockDim.y + threadIdx.y);
@@ -15,9 +16,9 @@ __global__ void phase_encoding(
 
     if (readout < echos.size(0) && voxel < echos.size(1)) {
         auto y = parameters.get(voxel).y;
-        auto k = trajectory.k_start[readout];
+        auto k = trajectory.k_start[readout].imag();
 
-        echos[readout][voxel] = exp(cfloat(0, y * k));
+        echos[readout][voxel] = exp(cfloat(0.0f, y * k));
     }
 }
 
