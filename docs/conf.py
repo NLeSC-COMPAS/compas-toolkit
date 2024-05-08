b197cb04f26a5172aad1cf392e1dc6dacf663a53
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2024-05-08 15:36:59 +0200

    Looking for another way to copy Julia documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -80,8 +80,6 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-# Copy the Julia documentation
-html_extra_path = ["api_julia"]
 
 
 # -- Breathe configuration -------------------------------------------------
