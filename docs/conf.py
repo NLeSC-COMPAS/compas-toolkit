7ebf4334f65c3936d0f26e3e4e108a7739f1d0f2
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2024-05-08 14:47:50 +0200

    Copy Julia documentation to GH page.

--- docs/conf.py
+++ docs/conf.py
@@ -80,6 +80,8 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+# Copy the Julia documentation
+html_extra_path = ["julia"]
 
 
 # -- Breathe configuration -------------------------------------------------
