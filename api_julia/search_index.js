2ecafb0f3e167bb8639bab5804bd304dc0064dfa
GitHub Action <action@github.com>
2025-04-28 19:35:51 +0000

    Adding generated Julia documentation

---
+++ api_julia/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CompasToolkit.jl","page":"CompasToolkit.jl","title":"CompasToolkit.jl","text":"","category":"section"},{"location":"","page":"CompasToolkit.jl","title":"CompasToolkit.jl","text":"Pages = [\"api_julia.md\"]","category":"page"},{"location":"","page":"CompasToolkit.jl","title":"CompasToolkit.jl","text":"Modules = [CompasToolkit]","category":"page"},{"location":"#CompasToolkit.CompasArray","page":"CompasToolkit.jl","title":"CompasToolkit.CompasArray","text":"Object representing an array of size N and type T.\n\n\n\n\n\n","category":"type"}]
+}
